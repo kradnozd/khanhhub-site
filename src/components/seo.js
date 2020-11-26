1adf223220707b872978d589fbb34743782d6daa
Tung Le <tungkradle@gmail.com>
2020-11-26 14:46:41 +0700

    unindex

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,6 +68,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `robots`,
+          content: `noindex, nofollow`,
+        },
       ].concat(meta)}
     />
   )
